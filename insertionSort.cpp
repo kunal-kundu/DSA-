878b1cadc372ab8aaea8a715d7ff790cc9237138
Kunal Kundu <96365401+kunal-kundu@users.noreply.github.com>
2025-05-05 16:13:39 +0530

    Create insertionSort.cpp
    Insertion Sort Program in easy steps.

---
+++ insertionSort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move element arr[j], that are
+           greater than key, to one position ahead(right side)
+           of their current position */
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
